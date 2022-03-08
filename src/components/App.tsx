0816737945558900e7544060c4aa89396a234d87
tim <hdamra@gmail.com>
2022-03-08 16:45:35 -0600

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../state';
+
+import RepositoriesList from './RepositoriesList';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+        <div>
+            <h1>Seach for a package: </h1>
+            <RepositoriesList />
+        </div>
+    </Provider>
+  )
+}
+
+export default App;
